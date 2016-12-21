9463ed34f9ca4b7d2f80d07fa2d423573ad45782
高尚 <gaoshang212@hotmail.com>
2016-12-21 15:18:31 +0800

    add Gsof

---
+++ Gsof.Shared/Extensions/BooleanExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gsof.Extensions
+{
+    public static class BooleanExtension
+    {
+        public static int ToInt(this bool p_bool)
+        {
+            return p_bool ? 1 : 0;
+        }
+
+        public static uint ToUint(this bool p_bool)
+        {
+            return (uint)p_bool.ToInt();
+        }
+    }
+}
---
+++ Gsof.Shared/Extensions/DateTimeExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gsof.Shared.Extensions
+{
+    public static class DateTimeExtension
+    {
+        public static long ToTimestamp(this DateTime p_dateTime)
+        {
+            return (long)(p_dateTime.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalMilliseconds;
+        }
+
+        public static DateTime ToDateTime(this long p_timestamp)
+        {
+            var ts = TimeSpan.FromMilliseconds(p_timestamp);
+            return new DateTime(1970, 1, 1, 0, 0, 0, 0).Add(ts);
+        }
+    }
+}
