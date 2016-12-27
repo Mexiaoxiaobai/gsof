1ab5ea859a88bd1ffc99ea93b84deece99abe8a8
gaoshang <gaoshang212@163.com>
2016-12-27 22:23:39 +0800

    add gsof.xaml.controls pack

--- Gsof.Xaml.Controls/Properties/AssemblyInfo.cs
+++ Gsof.Xaml.Controls/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Gsof.Xaml.Controls")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("a wpf controls library of gaoshang(gs)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gsof.Xaml.Controls")]
+[assembly: AssemblyCompany("gaoshang")]
+[assembly: AssemblyProduct("Gsof.Xaml")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
