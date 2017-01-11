967ca737535519371b2f95669338ae2f9b9678b7
高尚 <gaoshang212@hotmail.com>
2017-01-11 18:36:44 +0800

    add byteEx and md5Ex

---
+++ Gsof.Shared/Extensions/ByteExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gsof.Extensions
+{
+    public static class ByteExtension
+    {
+        public static string ToHex(this byte[] p_buffer)
+        {
+            var buffer = p_buffer;
+            if (buffer == null)
+            {
+                throw new ArgumentNullException("p_buffer");
+            }
+
+            var sb = new StringBuilder();
+            foreach (var b in buffer)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Gsof.Shared/Extensions/Md5Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Gsof.Extensions
+{
+    public static class Md5Extension
+    {
+        public static byte[] ToMd5(this byte[] p_buffer)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(p_buffer);
+            return hash;
+        }
+    }
+}
