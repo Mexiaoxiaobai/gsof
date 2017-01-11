a94b190253716b36f858eb5b47091293adafe40c
高尚 <gaoshang212@hotmail.com>
2017-01-11 19:13:34 +0800

    add PathExtension

---
+++ Gsof.Shared/Extensions/PathExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Gsof.Extensions
+{
+    public static class PathExtension
+    {
+        public static string GetAppData(params string[] p_paths)
+        {
+            var appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            return Combine(appdata, p_paths);
+        }
+
+        public static string GetLocalAppData(params string[] p_paths)
+        {
+            var appdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            return Combine(appdata, p_paths);
+        }
+
+        public static string Combine(string p_path, params string[] p_paths)
+        {
+            var paths = new[] { p_path }.Concat(p_paths).ToArray();
+            var dst = Path.Combine(paths);
+
+            return dst;
+        }
+
+    }
+}
