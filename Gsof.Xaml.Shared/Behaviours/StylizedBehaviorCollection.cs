c2a47bef66111fcfcb6f93834eaf46134a219379
gaoshang <gaoshang212@163.com>
2017-01-16 00:03:21 +0800

    add stylizedBehaviorCollection and StylizedBehaviors

---
+++ Gsof.Xaml.Shared/Behaviours/StylizedBehaviorCollection.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Gsof.Xaml.Behaviours
+{
+    public class StylizedBehaviorCollection : FreezableCollection<Behavior>
+    {
+        protected override Freezable CreateInstanceCore()
+        {
+            return new StylizedBehaviorCollection();
+        }
+    }
+}
