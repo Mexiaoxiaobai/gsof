88d4b069fbaf69710841f94c4a6fa2eeb97c36e2
高尚 <gaoshang212@hotmail.com>
2017-09-15 17:40:44 +0800

    add DictionaryExtension AddRange Method

---
+++ Gsof.Shared/Extensions/DictionaryExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Gsof.Shared.Extensions
+{
+    public static class DictionaryExtension
+    {
+        public static void AddRange<T1, T2, T3>(this IDictionary<T1, T2> p_dic, IDictionary<T1, T3> p_source) where T3 : T2
+        {
+            var dic = p_dic;
+            var source = p_source;
+            if (dic == null
+                || source == null)
+            {
+                return;
+            }
+
+            foreach (var kv in source)
+            {
+                dic.Add(kv.Key, kv.Value);
+            }
+        }
+    }
+}
