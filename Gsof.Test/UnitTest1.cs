367e9f86c297ffb0571ff02aba81fdf7d6ba97b3
高尚 <gaoshang212@hotmail.com>
2017-09-20 17:32:48 +0800

    add Process.Exec for task async get output

---
+++ Gsof.Test/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Gsof.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gsof.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task ProcessExec()
+        {
+            var process = new Process();
+
+            process.StartInfo.FileName = @"dmidecode.exe";
+            process.StartInfo.Arguments = "-s system-uuid";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+
+            var result = await process.Exec();
+            Assert.AreNotEqual(result.Trim(), string.Empty);
+        }
+
+        [TestMethod]
+        public async Task ProcessExExec()
+        {
+            var result = await ProcessEx.Exec(@"dmidecode.exe", "-s system-uuid");
+            Assert.AreNotEqual(result.Trim(), string.Empty);
+        }
+    }
+}
