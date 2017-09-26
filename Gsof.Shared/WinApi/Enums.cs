17889d95439e27050d2878e294d5c3a6162c94be
高尚 <gaoshang212@hotmail.com>
2017-09-26 16:13:09 +0800

    add winapi SetThreadExecutionState

---
+++ Gsof.Shared/WinApi/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gsof.Shared.WinApi
+{
+    [Flags]
+    public enum ThreadExecutionState : uint
+    {
+        System = 0x00000001,
+        Display = 0x00000002,
+        Continus = 0x80000000,
+    }
+}
---
+++ Gsof.Shared/WinApi/NativeMethods.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Gsof.Shared.WinApi
+{
+    internal class NativeMethods
+    {
+        [DllImport("kernel32.dll")]
+        internal static extern uint SetThreadExecutionState(uint esFlags);
+    }
+}
